e774543cc9028ae545d1e9047ef2f97d3f858070
charan panamata <charanpanamata@gmail.com>
2026-09-08 17:00:06 +0530

    Address review feedback for airport model

--- airstats/models/silver/silver_airports.sql
+++ airstats/models/silver/silver_airports.sql
@@ -3,4 +3,13 @@ with src_airports as (
     select *
     from {{ref('src_airports')}}
 )
-select * from src_airports
+select 
+    airport_ident,
+    airport_type,
+    airport_name,
+    airport_lat,
+    airport_long,
+    continent,
+    iso_country,
+    iso_region
+from src_airports
